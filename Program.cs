27ed85d01e6c10d62623410102c6c53e99757d69
566325 <nethisireesha288@gmail.com>
2023-09-29 17:50:24 +0530

    [Siresha]:Added Delegates and Events

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace DelegatesDemo
+{
+    public delegate int Operation(int x, int y);
+    internal class Program
+    {      
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Delegate Demo");
+
+            Operation delegateobj = new Operation(Addition);
+
+            int output = delegateobj.Invoke(2, 6);
+
+            Console.WriteLine("Addition is : " + output);
+
+            Console.WriteLine("*****************************");
+
+            MultiCastDelegate.ImplementDelegate();
+
+
+        }
+    }
+}
